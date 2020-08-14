84344fd564ef49abb4ead53a189c3025ff548002
nail1999 <nail1999@users.noreply.github.com>
2020-08-14 11:04:05 +0700

    add unit test projects

---
+++ ToDoApi/ToDoNunit/Controllers/ToDoesControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using ToDoApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApi.Controllers.Tests
+{
+    [TestFixture()]
+    public class ToDoesControllerTests
+    {
+        [Test()]
+        public void GetToDosTest()
+        {
+            
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetTodayToDosTest()
+        {
+            
+            
+            Assert.Fail();
+        }
+    }
+}
---
+++ ToDoApi/ToDoNunit/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using ToDoApi.Controllers;
+using ToDoApi.Models;
+
+namespace ToDoNunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCreateToDo()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
